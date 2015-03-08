43754850c410e7613b8f67eb1369d267bc7b679e
Pedro Tavares <p.miguel.tr@gmail.com>
2015-03-08 21:26:24 +0000

    NewBehaviour accepts slice of Behaviours, indentation fix

--- ambrosio.go
+++ ambrosio.go
@@ -2,43 +2,43 @@ package ambrosio
 
 import (
 	"fmt"
+	"github.com/codegangsta/negroni"
+	"github.com/gorilla/mux"
 	"net/http"
-    "regexp"
-    "github.com/codegangsta/negroni"
-    "github.com/gorilla/mux"
+	"regexp"
 )
 
 type Listener interface {
 	Listen(port int)
-    NewAction()
+	NewBehaviour()
 }
 
 type Ambrosio struct {
-	name    string
-    router  *mux.Router
-	negroni *negroni.Negroni
-    Behaviours []Behaviour
+	name       string
+	router     *mux.Router
+	negroni    *negroni.Negroni
+	Behaviours []Behaviour
 }
 
 type Behaviour struct {
-    Pattern string
-    Handler func([]string) (string, error)
+	Pattern string
+	Handler func([]string) (string, error)
 }
 
 type Handler func(http.ResponseWriter, *http.Request)
 
 func NewAmbrosio(name string) *Ambrosio {
-    /*helpBehaviour := Behaviour {
-        "/help",
-        func(matches []string, w http.ResponseWriter, req *http.Request) {
-            fmt.Println("help")
-        },
-    }*/
+	/*helpBehaviour := Behaviour {
+	    "/help",
+	    func(matches []string, w http.ResponseWriter, req *http.Request) {
+	        fmt.Println("help")
+	    },
+	}*/
 	ambrosio := Ambrosio{
 		name,
-        mux.NewRouter(),
+		mux.NewRouter(),
 		negroni.Classic(),
-        []Behaviour{},//helpBehaviour inside brackets
+		[]Behaviour{}, //helpBehaviour inside brackets
 	}
 	return &ambrosio
 }
@@ -46,25 +46,25 @@ func NewAmbrosio(name string) *Ambrosio {
 func (a Ambrosio) Listen(port int) {
 
 	a.router.HandleFunc("/ask", func(w http.ResponseWriter, req *http.Request) {
-        var handledOnce = false
+		var handledOnce = false
 		var actionResult string
 		var actionError error
-        action := req.FormValue("action")
+		action := req.FormValue("action")
 
-        for _, b := range a.Behaviours {
+		for _, b := range a.Behaviours {
 
-            matched, _ := regexp.MatchString(b.Pattern, action)
+			matched, _ := regexp.MatchString(b.Pattern, action)
 
-            if (matched == true) {
+			if matched == true {
 				matches := regexp.MustCompile(b.Pattern).FindStringSubmatch(action)
-                handledOnce = true
+				handledOnce = true
 				actionResult, actionError = b.Handler(matches)
-            }
+			}
 
-        }
-        if(!handledOnce) {
-            fmt.Fprintf(w, "Unkown command")
-        } else if(actionError == nil){
+		}
+		if !handledOnce {
+			fmt.Fprintf(w, "Unkown command")
+		} else if actionError == nil {
 			fmt.Fprintf(w, actionResult)
 		} else {
 			fmt.Fprintf(w, actionResult)
@@ -78,6 +78,6 @@ func (a Ambrosio) Listen(port int) {
 	n.Run(fmt.Sprintf(":%d", port))
 }
 
-func (a *Ambrosio) NewBehaviour(b Behaviour) {
-    a.Behaviours = append(a.Behaviours, b)
+func (a *Ambrosio) NewBehaviour(b ...Behaviour) {
+	a.Behaviours = append(a.Behaviours, b...)
 }
